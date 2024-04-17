c748166384f2eac29b6f7c4604e2d23fc74ece4d
André Schweiger <andre.spiele@yahoo.de>
2024-04-17 15:47:10 +0200

    Update license headers

--- src/main/java/me/andre111/voxedit/tool/ToolPaint.java
+++ src/main/java/me/andre111/voxedit/tool/ToolPaint.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 André Schweiger
+ * Copyright (c) 2024 André Schweiger
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
